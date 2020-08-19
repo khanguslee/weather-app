e72db350a7674ffbb0a1b91f87fa95dbeed44981
khanguslee <khanguslee@gmail.com>
2020-08-19 18:03:14 +1000

    Add server folder

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("../client/build"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
