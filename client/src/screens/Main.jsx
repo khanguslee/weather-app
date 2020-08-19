ed4ede3a83dd7103a4cb9607db3fed5fea623721
khanguslee <khanguslee@gmail.com>
2020-08-20 02:01:08 +1000

    Add app header

--- client/src/screens/Main.jsx
+++ client/src/screens/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import "./Main.scss";
+
 function Main() {
   return (
-    <div>
-      <h1>Weather</h1>
+    <div className="container">
+      <div className="header">
+        <h1>Weather</h1>
+      </div>
     </div>
   );
 }
