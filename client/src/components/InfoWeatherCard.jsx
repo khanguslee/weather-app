cd100b181538b2a3b445c89f5903a5dd8c0f8cc6
khanguslee <khanguslee@gmail.com>
2020-08-20 16:29:45 +1000

    Add stylign to infoweathercard component

--- client/src/components/InfoWeatherCard.jsx
+++ client/src/components/InfoWeatherCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./InfoWeatherCard.scss";
+
 function InfoWeatherCard(props) {
   const { humidity, pressure, rain, wind } = props;
   return (
