d6ba7d336eca139f8ff08322f6d9b876b95ae02d
Angus Lee <angus.lee809@gmail.com>
2020-08-21 14:57:12 +1000

    Store search data onto mongodb (#12)

--- server/controllers/weather.controller.js
+++ server/controllers/weather.controller.js
@@ -1,7 +1,12 @@
 const weatherService = require('../services/weather.service');
+const searchModel = require('../models/search');
 
 function getWeather(req, res, next) {
   const inputCity = req.params.cityName;
+  // Store search term
+  searchModel.create({ term: inputCity });
+
+  // Get weather data
   weatherService.getWeather(inputCity).then((data) => {
     res.send(data);
   });
